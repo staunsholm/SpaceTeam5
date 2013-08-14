ddd3a166c4efa6150803c7eb0b97f4826b1dcd95
Mikkel Staunsholm <crap2@staunsholm.dk>
2013-08-14 22:44:50 +0200

    grunt tasks for publishing client and server

--- Client/Network.js
+++ Client/Network.js
@@ -2,7 +2,7 @@ var Network = {};
 
 Network.joinGame = function (name) {
 
-    var socket = io.connect('127.0.0.1:8080');
+    var socket = io.connect('ec2-54-229-69-55.eu-west-1.compute.amazonaws.com:8080');
 
     socket.on('connect', function () {
         socket.emit('join', name);
--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,23 @@ module.exports = function (grunt) {
                 command: 'nodemon Server/Server.js'
             }
         },
+        sftp: {
+            'publish-server': {
+                files: {
+                    './': ['Server/**', 'package.json']
+                },
+                options: {
+                    path: '/usr/local/projects/SpaceTeam5/',
+                    username: 'root',
+                    //password: '',
+                    privateKey: grunt.file.read("./MikkelStaunsholm.pem"),
+                    //passphrase: '',
+                    host: 'ec2-54-229-69-55.eu-west-1.compute.amazonaws.com',
+                    createDirectories: true,
+                    directoryPermissions: parseInt(755, 8)
+                }
+            }
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -20,14 +37,41 @@ module.exports = function (grunt) {
                 src: 'src/<%= pkg.name %>.js',
                 dest: 'build/<%= pkg.name %>.min.js'
             }
+        },
+        ftpscript: {
+            'publish-client': {
+                options: {
+                    host: 'wsw6.surftown.dk',
+                    port: 21,
+                    passive: true
+                    // , type: 'ascii'
+                    // , mkdirs: false
+                    // , dryrun: true
+                    // , ftpCommand: 'ftp'
+                    // , encoding: 'utf-8'
+                    // , ftpEncoding: 'utf-8'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: '.',
+                        src: ['Client/**', 'node_modules/smokesignals/smokesignals.min.js'],
+                        dest: '/staunsholm/test.staunsholm.dk/SpaceTeam5'
+                    }
+                ]
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-ssh');
+    grunt.loadNpmTasks('grunt-ftpscript');
 
     // Default task(s).
     grunt.registerTask('default', []);
     grunt.registerTask('server', ['shell']);
+    grunt.registerTask('publish-server', ['sftp']);
+    grunt.registerTask('publish-client', ['ftpscript']);
 };
